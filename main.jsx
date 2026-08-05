e04648d630662d7fd4381c19c3490c99ffa6b544
Komeka71 <komudiekatpure17@gmail.com>
2026-08-05 23:07:13 +0530

    deploy change-1

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoaderPage from './LoaderPage'
+import LoaderPage from './client/src/components/LoaderPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
