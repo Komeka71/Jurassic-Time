cacbb1be06bc81b33bb8f6049b755895e55395e9
SHREYA PATIL <shreya@SHREYAs-MacBook-Air.local>
2026-08-08 03:43:33 +0530

    email2

--- server/utils/email.js
+++ server/utils/email.js
@@ -8,7 +8,7 @@ const getTransporter = () => {
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || "smtp.gmail.com",
       port: Number(process.env.EMAIL_PORT) || 465,
-      secure: false, // true for port 465
+      secure: true, // true for port 465
       family: 4, // force IPv4 - Render's IPv6 egress can't reach Gmail's SMTP host
       auth: {
         user: process.env.EMAIL_USER,
