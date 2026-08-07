a5683022bbd5e49d1c8f54c1d1fda331f3524456
SHREYA PATIL <shreya@SHREYAs-MacBook-Air.local>
2026-08-08 03:28:16 +0530

    merge uyfujhgfuyf

--- server/utils/email.js
+++ server/utils/email.js
@@ -9,6 +9,7 @@ const getTransporter = () => {
       host: process.env.EMAIL_HOST || "smtp.gmail.com",
       port: Number(process.env.EMAIL_PORT) || 587,
       secure: false, // true for port 465
+      family: 4, // force IPv4 - Render's IPv6 egress can't reach Gmail's SMTP host
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
