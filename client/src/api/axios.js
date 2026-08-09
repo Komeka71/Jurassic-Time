dcb5a346efdc9f86b9a8d2d84ed72c66bf5423f0
SHREYA PATIL <shreya@SHREYAs-MacBook-Air.local>
2026-08-09 12:33:36 +0530

    admin

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Automatically attach JWT to every request
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("jwt");
+const token = localStorage.getItem("jwt");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+if (token) {
+config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return config;
+return config;
 });
 
 export default api;
+
+// e.g. if baseURL is "http://localhost:3000/api", this gives "http://localhost:3000"
+export const SERVER_ORIGIN = api.defaults.baseURL.replace(/\/api\/?$/, "");
